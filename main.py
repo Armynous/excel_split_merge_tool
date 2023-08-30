58c76aa7f99f8dafe4be12982fb4c8c6aeed06ac
Armpongpol <86454087+ArmPongpon180@users.noreply.github.com>
2023-08-30 20:45:11 +0700

    maintain code

--- main.py
+++
@@ -1,4 +0,0 @@
-from main import ExcelSplitMergeTool
-
-split = ExcelSplitMergeTool()
-split.split_excel_file("data\Krungthai_Social_Aug2023_Page(R1).xlsx")
--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
-import os
 
-__version__ = 0.1
+__version__ = '0.0.1'
 __authur__ = "Pongpol Prommacharoen"
 __email__ = "pongpol095@gmail.com"
 __github__ = "https://github.com/Armynous"
 
-packages = list[str] = find_packages(exclude=['main', 'main.*'])
-
 #----------------------------------------------------------------------------#
 
-with open("requirments.txt", "r") as file_obj:
+with open("requirements.txt", "r") as file_obj:
 
     file_data = file_obj.read()
 
@@ -25,8 +22,8 @@ setup(
     version=__version__,
     author=__authur__,
     author_email=__email__,
-    packages=packages,
-    install_requires=dependencies,
+    packages=['main'],
+    install_requires=[],
     url='https://github.com/Armynous/split_n_merge_excelfile.git',
     include_package_data=True
 )
