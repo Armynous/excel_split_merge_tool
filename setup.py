1d30560fb77b3fb67e92e0c2506549faa990664c
Armpongpol <86454087+ArmPongpon180@users.noreply.github.com>
2023-08-30 23:21:25 +0700

    edit package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.0.2'
+__version__ = '0.0.4'
 __authur__ = "Pongpol Prommacharoen"
 __email__ = "pongpol095@gmail.com"
 __github__ = "https://github.com/Armynous"
@@ -16,7 +16,10 @@ setup(
     version=__version__,
     author=__authur__,
     author_email=__email__,
-    packages=['main'],
+    packages=find_packages(exclude=[
+        'Excel_split_merge_tool',
+        'Excel_split_merge_tool.*'
+    ]),
     url='https://github.com/Armynous/split_n_merge_excelfile.git',
     install_requires=[
         'tqdm',
