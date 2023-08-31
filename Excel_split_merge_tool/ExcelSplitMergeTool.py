926f0522bebd9ff00a57b7961a0e09c5d49a3764
Pongpol (Arm) Prommacharoen <pongpol@kalanth.com>
2023-08-31 16:34:43 +0700

    do readme.md and add more requirements

--- Excel_split_merge_tool/ExcelSplitMergeTool.py
+++ Excel_split_merge_tool/ExcelSplitMergeTool.py
@@ -12,9 +12,31 @@ class ExcelSplitMergeTool:
 
 # -------------------------------------------------------------------------- #
 
-    def split_excel_file(self, file_path:str) -> None:
+    def split_excel_file(self, file_path:str):
+        
+        """
+        Split an Excel (.xlsx) file into multiple smaller files
+        based on the specified number of output files
+    
+        This function reads an Excel file from the given 'file_path'
+        and divides its content into a specified number of output files, 
+        distributing the rows as evenly as possible. 
+        If there is a remainder in row distribution, 
+        the extra rows are distributed among the first few output files.
+        
+        Parameters:
+            file_path (str): The path to the input Excel file.
+        
+        Returns:
+            Excel file (.xlsx) file follow by NUM_OUPUT_FILE.
+            
+        Example:
+            splitter = ExcelSplitter()
+            splitter.split_excel_file("input.xlsx")
+        
+        """
 
-        file = self.read_excel(file_path)
+        file = pd.read_excel(file_path)
         num_output_file = Configs.NUM_OUPUT_FILE
 
         rows_per_file = file.shape[0] // num_output_file
@@ -36,7 +58,12 @@ class ExcelSplitMergeTool:
             data = file[start_idx:end_idx]
 
             file_name = os.path.splitext(os.path.basename(file_path))[0]
-            output_file_path = os.path.join(output_dir, f"{file_name}_split_{i+1}.xlsx")
+            
+            output_file_path = os.path.join(
+                output_dir, 
+                f"{file_name}_split_{i+1}.xlsx"
+            )
+            
             data.to_excel(output_file_path)
 
             print(f"Saved {output_file_path}")
@@ -45,9 +72,29 @@ class ExcelSplitMergeTool:
 
 # -------------------------------------------------------------------------- #
 
-    def merge_file(self):
+    def merge_file(self, folder_path:str):
+        
+        """
+        Merge multiple Excel files within a specified directory into a single combined Excel file.
+        
+        This function reads all Excel files within the provided 'folder_path', 
+        combines their contents row-wise, and saves
+        the combined data as a new Excel file named 'combined_file.xlsx' in a separate directorynamed 'merge_file'.
+        
+        Parameters:
+            folder_path (str): The path to the directory containing the Excel files to be merged.
+        
+        Returns:
+            None
+        
+        Example:
+            merger = ExcelMerger()
+            folder_path = "folder_with_excel_files"
+            merger.merge_file(folder_path)
+        
+        """
 
-        output_file = glob.glob(os.path.join("file_merge", "*.xlsx"))
+        output_file = glob.glob(os.path.join(folder_path, "*.xlsx"))
 
         excel_lst = []
 
@@ -61,13 +108,5 @@ class ExcelSplitMergeTool:
         combined.to_excel(output_file_path, index=False)
 
         print("Merge file Successful")
-    
-# -------------------------------------------------------------------------- #
-
-    def read_excel(self, file_path:str):
-        
-        path = pd.read_excel(file_path)
-
-        return path
     
 # -------------------------------------------------------------------------- #
