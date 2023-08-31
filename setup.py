f9f4c80a77be10e313ca3efdc04672544d300bcf
Pongpol (Arm) Prommacharoen <pongpol@kalanth.com>
2023-08-31 12:20:18 +0700

    edit setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 __authur__ = "Pongpol Prommacharoen"
 __email__ = "pongpol095@gmail.com"
 __github__ = "https://github.com/Armynous"
@@ -16,9 +16,9 @@ setup(
     version=__version__,
     author=__authur__,
     author_email=__email__,
-    packages=find_packages(include=[
-        'Excel_split_merge_tool',
-    ]),
+    packages=['Excel_split_merge_tool'],
+    package_dir={'Excel_split_merge_tool': 'split_n_merge_file/Excel_split_merge_tool'},
+    package_data={'Excel_split_merge_tool': ['resources/*.json']},
     url='https://github.com/Armynous/split_n_merge_excelfile.git',
     install_requires=[
         'tqdm',
@@ -26,8 +26,6 @@ setup(
         'pandas>=2.1.0',
         'numpy>=1.25.2'
     ],
-    package_dir={'Excel_split_merge_tool': 'Excel_split_merge_tool'},
-    package_data={'Excel_split_merge_tool': ['resource/*.json']},
     include_package_data=True
 )
 
